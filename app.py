c1175e185619dd4c1b337d5fd5fc346dc5f5546e
Enzo <enzocuellar12@gmail.com>
2021-04-11 23:09:48 -0300

    Add first functionalities

--- app.py
+++ app.py
@@ -1,6 +1,45 @@
 from tkinter import *
+from tkinter import messagebox
+import sqlite3
 
 root = Tk()
+
+idValue = StringVar()
+nameValue = StringVar()
+passwordValue = StringVar()
+surnameValue = StringVar()
+addressValue = StringVar()
+commentsValue = StringVar()
+db_connection = sqlite3.connect('crudBD')
+def connection():
+
+    try:
+
+
+        db_connection.execute("""CREATE TABLE users_data (
+                        id int(60) PRIMARY_KEY AUTO_INCREMENT,
+                        ) """)
+
+    except ConnectionError:
+        messagebox.ERROR("something goes bad")
+
+def exitProgram():
+    close = messagebox.askokcancel("Are you sure you want to exit?", "If you press ok program will close")
+
+    if close:
+        root.destroy()
+
+
+def clearFields():
+    idValue.set("")
+    nameValue.set("")
+    passwordValue.set("")
+    surnameValue.set("")
+    addressValue.set("")
+    commentsValue.set("")
+
+
+
 
 # -----menu--------
 
@@ -8,16 +47,16 @@ menuBar = Menu(root)
 
 dbMenu = Menu(menuBar, tearoff=0)
 
-dbMenu.add_command(label="Connect")
-dbMenu.add_command(label="Exit")
+dbMenu.add_command(label="Connect", command=lambda: connection)
+dbMenu.add_command(label="Exit", command=exitProgram)
 
 deleteMenu = Menu(menuBar, tearoff=0)
 
-deleteMenu.add_command(label="Delete fields")
+deleteMenu.add_command(label="Delete fields", command=clearFields)
 
 crudMenu = Menu(menuBar, tearoff=0)
 
-crudMenu.add_command(label="Create")
+crudMenu.add_command(label="Create", command=lambda: create)
 crudMenu.add_command(label="Read")
 crudMenu.add_command(label="Update")
 crudMenu.add_command(label="Delete")
@@ -41,11 +80,11 @@ surname = Label(text="surname").grid(row=4, column=2, pady=9, padx=9)
 address = Label(text="address").grid(row=5, column=2, pady=9, padx=9)
 comments = Label(text="comments").grid(row=6, column=2, pady=9, padx=9)
 
-idEntry = Entry(root).grid(row=1, column=3, pady=9, padx=9)
-nameEntry = Entry(root).grid(row=2, column=3)
-passwordEntry = Entry(root).grid(row=3, column=3)
-surnameEntry = Entry(root).grid(row=4, column=3)
-addressEntry = Entry(root).grid(row=5, column=3)
+idEntry = Entry(root, textvariable=idValue).grid(row=1, column=3, pady=9, padx=9)
+nameEntry = Entry(root, textvariable=nameValue).grid(row=2, column=3)
+passwordEntry = Entry(root, textvariable=passwordValue, show="*").grid(row=3, column=3)
+surnameEntry = Entry(root, textvariable=surnameValue).grid(row=4, column=3)
+addressEntry = Entry(root, textvariable=addressValue).grid(row=5, column=3)
 commentsEntry = Text(root, width=18, height=5).grid(row=6, column=3)
 
 
